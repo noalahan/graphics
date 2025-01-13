2f86c04afb08e0e4639a56617cc98622066d96ed
Noa Lahan <n.lahan0720231@arts.ac.uk>
2025-01-13 12:00:46 -0800

    1 vect

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -1,19 +1,50 @@
-function main(){
-    // retrieve canvas element
-    var canvas = document.getElementById('canvas');
-    if (!canvas) {
-        console.log('Failed to retrieve the <canvas> element');
-        return;
-    }
+var ctx, canvas;
+function main() {
+  // retrieve canvas element
+  canvas = document.getElementById("canvas");
+  if (!canvas) {
+    console.log("Failed to retrieve the <canvas> element");
+    return;
+  }
+  canvas.style.backgroundColor = "black";
 
-    // get the rendering context for 2DCG
-    var ctx = canvas.getContext('2d');
+  // get the rendering context for 2DCG
+  ctx = canvas.getContext("2d");
 
-    // draw the rectangle
-    ctx.fillStyle = 'rgba(255, 0, 0, 1.0)'; // sets color to blue
-    ctx.fillRect(20, 0, 40, 80);        // fill rect with color
-    ctx.fillRect(80, 0, 40, 80);        // fill rect with color
-    ctx.fillRect(0, 20, 140, 40);        // fill rect with color
-    ctx.fillRect(40, 60, 60, 40);        // fill rect with color
-    ctx.fillRect(60, 100, 20, 20);        // fill rect with color
+  // draw the rectangle
+  //ctx.fillStyle = "rgba(0, 0, 0, 1.0)"; // sets color to blue
+  //ctx.fillRect(0, 0, 400, 400); // fill rect with color
+
+  const v1 = new Vector3([2.25, 2.25, 0]);
+  drawVector(v1, "red");
+}
+
+/**
+ * Draws input vector
+ * @param {Vector3} v draws a vector in the canvas
+ * @param {string} color color to draw the vector in
+ */
+function drawVector(v, color) {
+  // get coordinates
+  let x = v.elements[0];
+  let y = v.elements[1];
+
+  // Start a new Path
+  ctx.beginPath();
+  ctx.moveTo(200, 200);
+  ctx.lineTo(200 + 20 * x, 200 - 20 * y);
+
+  // Draw the Path
+  ctx.strokeStyle = color;
+  ctx.stroke();
+}
+
+function handleDrawEvent() {
+  var vect = document.getElementById("v1");
+  let x = vect.elements[0].value;
+  let y = vect.elements[1].value;
+  const v1 = new Vector3([x, y, 0]);
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawVector(v1, "red");
 }
