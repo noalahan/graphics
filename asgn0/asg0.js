f7c9895a0f607b1c975b0ed0350b032d4d3e2e09
Noa Lahan <n.lahan0720231@arts.ac.uk>
2025-01-13 13:55:29 -0800

    operations done

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -29,6 +29,7 @@ function drawVector(v, color) {
 
   // Draw the Path
   ctx.strokeStyle = color;
+  ctx.lineWidth = 3;
   ctx.stroke();
 }
 
@@ -36,18 +37,67 @@ function drawVector(v, color) {
  * Processes input data into vectors to be drawn
  */
 function handleDrawEvent() {
-  var vect = document.getElementById("form");
+  // set vector 1
+  var vect = document.getElementById("vect_form");
   let x = vect.elements[0].value;
   let y = vect.elements[1].value;
   const v1 = new Vector3([x, y, 0]);
 
+  // set vector 2
   x = vect.elements[2].value;
   y = vect.elements[3].value;
   const v2 = new Vector3([x, y, 0]);
 
+  // clear canvas and draw
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawVector(v1, "red");
   drawVector(v2, "blue");
 }
 
+function handleDrawOperationEvent() {
+  // get vectors
+  var vect = document.getElementById("vect_form");
+  let x = vect.elements[0].value;
+  let y = vect.elements[1].value;
+  const v1 = new Vector3([x, y, 0]);
+  x = vect.elements[2].value;
+  y = vect.elements[3].value;
+  const v2 = new Vector3([x, y, 0]);
 
+  // draw vectors
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawVector(v1, "red");
+  drawVector(v2, "blue");
+
+  // get operation
+  var ops = document.getElementById("ops_form");
+  let operation = ops.elements[0].value;
+  let s = ops.elements[1].value;
+
+  // do operation
+  if (operation == "add") {
+    v1.add(v2);
+    drawVector(v1, "green");
+  } else if (operation == "sub") {
+    v1.sub(v2);
+    drawVector(v1, "green");
+  } else if (operation == "mul") {
+    v1.mul(s);
+    v2.mul(s);
+    drawVector(v1, "green");
+    drawVector(v2, "green");
+  } else if (operation == "div") {
+    v1.div(s);
+    v2.div(s);
+    drawVector(v1, "green");
+    drawVector(v2, "green");
+  } else if (operation == "mag") {
+    console.log("Magnitude v1: " + v1.magnitude());
+    console.log("Magnitude v2: " + v2.magnitude());
+  } else if (operation == "nor") {
+    v1.normalize();
+    v2.normalize();
+    drawVector(v1, "green");
+    drawVector(v2, "green");
+  }
+}
