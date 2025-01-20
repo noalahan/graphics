526a515336bccdfedeec668dfbbfb2ceca42befe
Noa Lahan <n.lahan0720231@arts.ac.uk>
2025-01-19 22:28:42 -0800

    drawing

--- asgn1/asg1.js
+++ asgn1/asg1.js
@@ -70,6 +70,7 @@ let g_selectedSize = 5;
 let g_selectedType = POINT;
 let g_selectedSeg = 10;
 
+let r = 255, g = 255, b = 255;
 function addActionsForHtmlUI() {
   // Clear canvas
   document.getElementById("clear").onclick = function () {
@@ -106,6 +107,8 @@ function addActionsForHtmlUI() {
   document.getElementById("seg").addEventListener("mouseup", function () {
     g_selectedSeg = this.value;
   });
+
+  document.getElementById("draw").onclick = painting();
 }
 
 function main() {
@@ -128,7 +131,7 @@ function main() {
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  // drawTriangles([0,0.5, -0.5,-0.5,  0.5,-0.5]);
+  // gl.drawArrays(gl.POINTS, 0, n);
 }
 
 function convertCoordinatesEventToGL(ev) {
@@ -159,9 +162,9 @@ function click(ev) {
   [x, y] = convertCoordinatesEventToGL(ev);
 
   let point;
-  if (g_selectedType == POINT){
+  if (g_selectedType == POINT) {
     point = new Point();
-  } else if (g_selectedType == TRIANGLE){
+  } else if (g_selectedType == TRIANGLE) {
     point = new Triangle();
   } else {
     point = new Circle();
@@ -175,32 +178,102 @@ function click(ev) {
   renderAllShapes();
 }
 
-// function drawTriangles(vertices) {
-//   // var vertices = new Float32Array([0, 0.5, -0.5, -0.5, 0.5, -0.5]);
-//   var n = 3; // The number of vertices
+function draw(vertices) {
+  var n = 3; // The number of vertices
 
-//   // Create a buffer object
-//   var vertexBuffer = gl.createBuffer();
-//   if (!vertexBuffer) {
-//     console.log("Failed to create the buffer object");
-//     return -1;
-//   }
+  // Create a buffer object
+  var vertexBuffer = gl.createBuffer();
+  if (!vertexBuffer) {
+    console.log("Failed to create the buffer object");
+    return -1;
+  }
 
-//   // Bind the buffer object to target
-//   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-//   // Write date into the buffer object
-//   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  // Bind the buffer object to target
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  // Write date into the buffer object
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
-//   var a_Position = gl.getAttribLocation(gl.program, "a_Position");
-//   if (a_Position < 0) {
-//     console.log("Failed to get the storage location of a_Position");
-//     return -1;
-//   }
-//   // Assign the buffer object to a_Position variable
-//   gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+  // var a_Position = gl.getAttribLocation(gl.program, "a_Position");
+  // if (a_Position < 0) {
+  //   console.log("Failed to get the storage location of a_Position");
+  //   return -1;
+  // }
+  // Assign the buffer object to a_Position variable
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
 
-//   // Enable the assignment to a_Position variable
-//   gl.enableVertexAttribArray(a_Position);
+  // Enable the assignment to a_Position variable
+  gl.enableVertexAttribArray(a_Position);
 
-//   gl.drawArrays(gl.TRIANGLES, 0, n);
-// }
+  gl.uniform4f(u_FragColor, color[0], color[1], color[2], color[3]);
+
+  gl.drawArrays(gl.TRIANGLES, 0, n);
+}
+
+let color;
+function painting() {
+  // background
+  color = [169 / 255, 214 / 255, 232 / 255, 1.0];
+  draw([-1, -1, 1, -1, 1, 1]);
+  draw([-1, -1, -1, 1, 1, 1]);
+
+  // plane
+  color = [248 / 255, 186 / 255, 18 / 255, 1.0];
+  draw([-0.8, 0.5, -0.6, 0.2, 0.8, 0.1]);
+  color = [229 / 255, 153 / 255, 24 / 255, 1.0];
+  draw([-0.35, -0.3, -0.6, -0.4, -0.4, -0.1]);
+  draw([-0.8, 0.5, -0.15, 0.15, -0.1, 0.15]);
+  color = [210 / 255, 119 / 255, 29 / 255, 1.0];
+  draw([-0.6, 0.2, -0.4, -0.1, 0.8, 0.1]);
+  draw([-0.6, 0.2, -0.6, -0.4, -0.4, -0.1]);
+
+  // coat
+  color = [29 / 255, 62 / 255, 78 / 255, 1.0];
+  draw([0.4, 0.1, 0.45, 0, 0.45, 0.3]);
+  draw([0.6, 0, 0.45, 0, 0.45, 0.3]);
+  draw([0.6, 0, 0.6, 0.3, 0.45, 0.3]);
+  draw([0.6, 0, 0.6, 0.3, 0.65, 0.05]);
+
+  // head
+  color = [255 / 255, 202 / 255, 133 / 255, 1.0];
+  draw([0.5, 0.45, 0.675, 0.4, 0.675, 0.5]);
+  draw([0.5, 0.45, 0.675, 0.4, 0.6, 0.35]);
+  draw([0.5, 0.45, 0.45, 0.35, 0.6, 0.35]);
+  draw([0.5, 0.3, 0.45, 0.35, 0.6, 0.35]);
+  color = [134 / 255, 67 / 255, 9 / 255, 1.0];
+  draw([0.5, 0.45, 0.375, 0.5, 0.4, 0.55]);
+  draw([0.5, 0.45, 0.45, 0.58, 0.4, 0.55]);
+  draw([0.5, 0.45, 0.45, 0.58, 0.5, 0.6]);  //?
+  draw([0.5, 0.45, 0.55, 0.6, 0.5, 0.6]);
+  draw([0.5, 0.45, 0.55, 0.6, 0.6, 0.58]);
+  draw([0.5, 0.45, 0.65, 0.55, 0.6, 0.58]);
+  draw([0.5, 0.45, 0.65, 0.55, 0.675, 0.5]);
+  draw([0.5, 0.45, 0.375, 0.5, 0.475, 0.35]);
+  draw([0.36, 0.44, 0.375, 0.5, 0.475, 0.35]);
+  draw([0.36, 0.44, 0.3, 0.4, 0.475, 0.35]);
+  draw([0.4, 0.35, 0.3, 0.4, 0.475, 0.35]);
+
+  // goggles
+  color = [58 / 255, 28 / 255, 4 / 255, 1.0];
+  draw([0.69, 0.43, 0.69, 0.49, 0.35, 0.45])
+  color = [96 / 255, 48 / 255, 6 / 255, 1.0];
+  draw([0.6, 0.5, 0.69, 0.49, 0.6, 0.42])
+  draw([0.69, 0.43, 0.69, 0.49, 0.6, 0.42])
+  color = [207 / 255, 232 / 255, 242 / 255, 1.0];
+  draw([0.61, 0.49, 0.68, 0.48, 0.61, 0.43])
+  draw([0.68, 0.44, 0.68, 0.48, 0.61, 0.43])
+
+  // scarf
+  color = [171 / 255, 52 / 255, 40 / 255, 1.0];
+  draw([0.45, 0.3, 0.45, 0.35, 0.55, 0.275]);
+  draw([0.45, 0.3, 0.6, 0.3, 0.55, 0.275]);
+  draw([0.45, 0.3, 0.6, 0.3, 0.6, 0.35]);
+  draw([0.45, 0.3, 0.45, 0.35, 0.35, 0.25]);
+  draw([0.2, 0.3, 0.2, 0.4, 0.35, 0.25]);
+  draw([0.3, 0.3, 0.4, 0.3, 0.35, 0.25]);
+
+  // plane top wing
+  color = [248 / 255, 186 / 255, 18 / 255, 1.0];
+  draw([-0.4, -0.1, -0.3, -0.5, 0.8, 0.1]);
+  color = [229 / 255, 153 / 255, 24 / 255, 1.0];
+  draw([-0.1, -0.05, -0.3, -0.5, -0.065, -0.04]);
+}
