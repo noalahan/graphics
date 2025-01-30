a9ec7b13db619b95f5c0d9ed05d20085c7a5a3f2
Noa Lahan <n.lahan0720231@arts.ac.uk>
2025-01-30 00:16:06 -0800

    starting

--- asgn2/Cube.js
+++ asgn2/Cube.js
@@ -57,3 +57,29 @@ class Cube {
     drawTriangle3D([0, 0, 0, 0, 1, 1, 0, 0, 1]);
   }
 }
+
+function drawTriangle3D(vertices) {
+  // var vertices = new Float32Array([0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+  var n = 3; // The number of vertices
+
+  // Create a buffer object
+  var vertexBuffer = gl.createBuffer();
+  if (!vertexBuffer) {
+    console.log("Failed to create the buffer object");
+    return -1;
+  }
+
+  // Bind the buffer object to target
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  // Write date into the buffer object
+  //   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
+
+  // Assign the buffer object to a_Position variable
+  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+
+  // Enable the assignment to a_Position variable
+  gl.enableVertexAttribArray(a_Position);
+
+  gl.drawArrays(gl.TRIANGLES, 0, n);
+}
--- asgn2/asg2.js
+++ asgn2/asg2.js
@@ -81,32 +81,25 @@ function connectVariablesToGLSL() {
   gl.uniformMatrix4fv(u_ModelMatrix, false, identityM.elements);
 }
 
-const POINT = 0;
-const TRIANGLE = 1;
-const CIRCLE = 2;
-
-let g_selectedColor = [1.0, 1.0, 1.0, 1.0];
-let g_selectedSize = 5;
-let g_selectedType = POINT;
-let g_selectedSeg = 10;
 let g_globalAngle = 0;
+let g_yellowAngle = 0;
+let g_pinkAngle = 0;
+let g_yellowAnim = false;
+let g_pinkAnim = false;
 
 function addActionsForHtmlUI() {
-  // Clear canvas
-  document.getElementById("clear").onclick = function () {
-    g_shapesList = [];
-    renderAllShapes();
+  // animation selector
+  document.getElementById("yon").onclick = function () {
+    g_yellowAnim = true;
   };
-
-  // shape selector
-  document.getElementById("sqr").onclick = function () {
-    g_selectedType = POINT;
+  document.getElementById("yoff").onclick = function () {
+    g_yellowAnim = false;
   };
-  document.getElementById("tri").onclick = function () {
-    g_selectedType = TRIANGLE;
+  document.getElementById("pon").onclick = function () {
+    g_pinkAnim = true;
   };
-  document.getElementById("cir").onclick = function () {
-    g_selectedType = CIRCLE;
+  document.getElementById("poff").onclick = function () {
+    g_pinkAnim = false;
   };
 
   // rotation selector
@@ -114,24 +107,13 @@ function addActionsForHtmlUI() {
     g_globalAngle = this.value;
     renderAllShapes();
   });
-
-  // Color selector
-  document.getElementById("r").addEventListener("mouseup", function () {
-    g_selectedColor[0] = this.value / 100;
+  document.getElementById("yellow").addEventListener("mousemove", function () {
+    g_yellowAngle = this.value;
+    renderAllShapes();
   });
-  document.getElementById("g").addEventListener("mouseup", function () {
-    g_selectedColor[1] = this.value / 100;
-  });
-  document.getElementById("b").addEventListener("mouseup", function () {
-    g_selectedColor[2] = this.value / 100;
-  });
-
-  // size selector
-  document.getElementById("size").addEventListener("mouseup", function () {
-    g_selectedSize = this.value;
-  });
-  document.getElementById("seg").addEventListener("mouseup", function () {
-    g_selectedSeg = this.value;
+  document.getElementById("pink").addEventListener("mousemove", function () {
+    g_pinkAngle = this.value;
+    renderAllShapes();
   });
 }
 
@@ -140,83 +122,79 @@ function main() {
   connectVariablesToGLSL();
   addActionsForHtmlUI();
 
-  // Register function (event handler) to be called on a mouse press
-  canvas.onmousedown = click;
-  canvas.onmousemove = function (ev) {
-    if (ev.buttons == 1) {
-      click(ev);
-    }
-  };
-
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
   // Render
-  renderAllShapes();
+  // renderAllShapes();
+  requestAnimationFrame(tick);
 }
 
-function convertCoordinatesEventToGL(ev) {
-  var x = ev.clientX; // x coordinate of a mouse pointer
-  var y = ev.clientY; // y coordinate of a mouse pointer
-  var rect = ev.target.getBoundingClientRect();
-
-  x = (x - rect.left - canvas.width / 2) / (canvas.width / 2);
-  y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
-
-  return [x, y];
+function updateAnim() {
+  if (g_yellowAnim) {
+    g_yellowAngle = 45 * Math.sin(g_seconds);
+  }
+  if (g_pinkAnim) {
+    g_pinkAngle = 45 * Math.sin(3*g_seconds);
+  }
 }
 
 // draw every shape on canvas
 function renderAllShapes() {
-   // pass the matrix to u_ModelMatrix attribute
+  // pass the matrix to u_ModelMatrix attribute
   var globalRotMat = new Matrix4().rotate(g_globalAngle, 0, 1, 0);
   gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
 
   // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   // draw the body cube
-  var body = new Cube();
-  body.color = [1.0, 0.0, 0.0, 1.0];
-  body.matrix.setTranslate(-0.25, -0.5, 0.0);
-  body.matrix.scale(0.5, 1, 0.5);
-  body.render();
+  var red = new Cube();
+  red.color = [1.0, 0.0, 0.0, 1.0];
+  red.matrix.setTranslate(-0.25, -0.75, 0.0);
+  red.matrix.rotate(-5, 1, 0, 0);
+  red.matrix.scale(0.5, 0.3, 0.5);
+  red.render();
 
   // draw left arm
-  var leftArm = new Cube();
-  leftArm.color = [1, 1, 0, 1];
-  leftArm.matrix.setTranslate(0.7, 0.0, 0.0);
-  leftArm.matrix.rotate(45, 0, 0, 1);
-  leftArm.matrix.scale(0.25, 0.7, 0.5);
-  leftArm.render();
+  var yellow = new Cube();
+  yellow.color = [1, 1, 0, 1];
+  yellow.matrix.setTranslate(0, -0.5, 0.0);
+  yellow.matrix.rotate(-5, 1, 0, 0);
+  yellow.matrix.rotate(-g_yellowAngle, 0, 0, 1);
+  var yellowCoordinates = new Matrix4(yellow.matrix);
+  yellow.matrix.scale(0.25, 0.7, 0.5);
+  yellow.matrix.translate(-0.5, 0, 0);
+  yellow.render();
 
   // test box
-  var box = new Cube();
-  box.color = [1, 0, 1, 1];
-  box.matrix.translate(0, 0, -0.50, 0);
-  box.matrix.rotate(-30, 1, 0, 0);
-  box.matrix.scale(.5, .5, .5);
-  box.render();
+  var pink = new Cube();
+  pink.color = [1, 0, 1, 1];
+  pink.matrix = yellowCoordinates;
+  pink.matrix.translate(0, 0.65, 0);
+  pink.matrix.rotate(-g_pinkAngle, 0, 0, 1);
+  pink.matrix.scale(0.3, 0.3, 0.3);
+  pink.matrix.translate(-0.5, 0, -0.00005);
+  pink.render();
+}
+
+var g_startTime = performance.now() / 1000.0;
+var g_seconds = performance.now() / 1000.0;
+
+function tick() {
+  // save current time
+  g_seconds = performance.now() / 1000.0 - g_startTime;
+  console.log(g_seconds);
+
+  // update animation angles
+  updateAnim();
+
+  // Draw everything
+  renderAllShapes();
+
+  // Tell the browser to update again
+  requestAnimationFrame(tick);
 }
 
 var g_shapesList = [];
-
-function click(ev) {
-  [x, y] = convertCoordinatesEventToGL(ev);
-
-  let point;
-  if (g_selectedType == POINT) {
-    point = new Point();
-  } else if (g_selectedType == TRIANGLE) {
-    point = new Triangle();
-  } else {
-    point = new Circle();
-    point.segments = g_selectedSeg;
-  }
-  point.position = [x, y];
-  point.color = g_selectedColor.slice();
-  point.size = g_selectedSize;
-  g_shapesList.push(point);
-
-  renderAllShapes();
-}
