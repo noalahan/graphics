4e36c4012fc3489835ca173c3ae596c4daef77d8
Noa Lahan <n.lahan0720231@arts.ac.uk>
2025-03-09 19:26:43 -0700

    three proj

---
+++ asgn5/src/main.js
@@ -0,0 +1,168 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { OBJLoader } from "three/addons/loaders/OBJLoader.js";
+
+let renderer;
+let camera;
+let scene;
+let loadManager;
+let loader;
+
+let materials;
+let cubes;
+
+main();
+function main() {
+  // set up
+  sceneSetup();
+  lighting();
+
+  textureSetup();
+
+  // create objects
+  createCubes();
+  objectLoaders();
+
+  // render
+  requestAnimationFrame(render);
+}
+
+/**
+ * Sets up canvas, renderer, camera, and scene
+ */
+function sceneSetup() {
+  let w = window.innerWidth * 0.8;
+  let h = window.innerHeight * 0.3;
+  // set up canvas
+  const canvas = document.querySelector("#c");
+  renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+  renderer.setSize(w, h);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  // set up camera
+  const fov = 90;
+  const aspect = w / h;
+  const near = 0.1;
+  const far = 100;
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 5, 10);
+
+  // resize canvas if window size changes
+  window.addEventListener("resize", () => {
+    w = window.innerWidth;
+    h = window.innerHeight;
+    camera.aspect = w / h;
+    camera.updateProjectionMatrix();
+    renderer.setSize(w, h);
+  });
+
+  // create camera controls
+  const controls = new OrbitControls(camera, canvas);
+  controls.target.set(0, 5, 0);
+  controls.update();
+
+  // set up scene
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color("skyblue");
+}
+
+/**
+ * Adds lighting to Scene
+ */
+function lighting() {
+  // create light
+  const color = 0xffffff;
+  const intensity = 3;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(-1, 2, 4);
+  scene.add(light);
+}
+
+function textureSetup(){
+  loadManager = new THREE.LoadingManager();
+  loader = new THREE.TextureLoader();
+
+  // cube
+  materials = [
+    new THREE.MeshBasicMaterial({ map: loadColorTexture("img/hedge1.png") }),
+    new THREE.MeshBasicMaterial({ map: loadColorTexture("img/hedge2.png") }),
+    new THREE.MeshBasicMaterial({ map: loadColorTexture("img/hedge3.png") }),
+    new THREE.MeshBasicMaterial({ map: loadColorTexture("img/hedge4.png") }),
+    new THREE.MeshBasicMaterial({ map: loadColorTexture("img/hedge5.png") }),
+    new THREE.MeshBasicMaterial({ map: loadColorTexture("img/hedge6.png") }),
+  ];
+
+  function loadColorTexture(path) {
+    const texture = loader.load(path);
+    texture.colorSpace = THREE.SRGBColorSpace;
+    return texture;
+  }
+}
+
+function createCubes() {
+  // create box
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+  cubes = [
+    makeInstance(geometry, 0x44aa88, 0),
+    makeInstance(geometry, 0x8844aa, -2),
+    makeInstance(geometry, 0xaa8844, 2),
+  ];
+
+  function makeInstance(geometry, color, x) {
+    // const material = new THREE.MeshPhongMaterial({ color });
+    // const material = new THREE.MeshBasicMaterial({ map: texture });
+    const cube = new THREE.Mesh(geometry, materials);
+    scene.add(cube);
+    cube.position.x = x;
+    return cube;
+  }
+}
+
+function objectLoaders() {
+  // obj loader
+  const objLoader = new OBJLoader();
+  objLoader.load(
+    "https://threejs.org/manual/examples/resources/models/windmill/windmill.obj",
+    (root) => {
+      scene.add(root);
+    }
+  );
+}
+
+function render(time) {
+  time *= 0.001; // convert time to seconds
+
+  cubes.forEach((cube, ndx) => {
+    const speed = 1 + ndx * 0.1;
+    const rot = time * speed;
+    cube.rotation.x = rot;
+    cube.rotation.y = rot;
+  });
+
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(render);
+}
+
+// loadProgress();
+// function loadProgress() {
+//   const loadingElem = document.querySelector("#loading");
+//   const progressBarElem = loadingElem.querySelector(".progressbar");
+
+//   loadManager.onLoad = () => {
+//     loadingElem.style.display = "none";
+//     const cube = new THREE.Mesh(geometry, materials);
+//     scene.add(cube);
+//     cubes.push(cube); // add to our list of cubes to rotate
+//   };
+
+//   loadManager.onProgress = (urlOfLastItemLoaded, itemsLoaded, itemsTotal) => {
+//     const progress = itemsLoaded / itemsTotal;
+//     progressBarElem.style.transform = `scaleX(${progress})`;
+//   };
+// }
