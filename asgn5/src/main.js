512655db42c2e1a577a5ff3a5fc2d892d91f0f3c
Noa Lahan <n.lahan0720231@arts.ac.uk>
2025-03-12 01:43:02 -0700

    deployment 10

--- asgn5/src/main.js
+++ asgn5/src/main.js
@@ -1,9 +1,9 @@
 // most base code from https://threejs.org/manual
 import "./style.css";
-import * as THREE from "/node_modules/three/build/three.module.js";
-import { OrbitControls } from "/node_modules/three/examples/jsm/controls/OrbitControls.js";
-import { OBJLoader } from "/node_modules/three/examples/jsm/loaders/OBJLoader.js";
-import { MTLLoader } from "/node_modules/three/examples/jsm/loaders/MTLLoader.js";
+import * as THREE from "./node_modules/three/build/three.module.js";
+import { OrbitControls } from "./node_modules/three/examples/jsm/controls/OrbitControls.js";
+import { OBJLoader } from "./node_modules/three/examples/jsm/loaders/OBJLoader.js";
+import { MTLLoader } from "./node_modules/three/examples/jsm/loaders/MTLLoader.js";
 
 let renderer;
 let camera;
