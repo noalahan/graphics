947669c630f2c0e749a8cd34f3e3ba24d2fb1a7f
Noa Lahan <n.lahan0720231@arts.ac.uk>
2025-03-12 00:42:02 -0700

    deploying 2

--- asgn5/src/main.js
+++ asgn5/src/main.js
@@ -1,6 +1,6 @@
 // most base code from https://threejs.org/manual
 import "./style.css";
-import * as THREE from "/node_modules/three/build/three.module.js";
+import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { OBJLoader } from "three/addons/loaders/OBJLoader.js";
 import { MTLLoader } from "three/addons/loaders/MTLLoader.js";
